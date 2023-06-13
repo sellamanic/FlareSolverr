6ca5f177337bc9c454743b7696e188ecf0cf42a5
sellamanic <108454368+sellamanic@users.noreply.github.com>
2023-06-13 18:37:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 import codecs
 import os
 import re
-
+from pathlib import Path
 from setuptools import setup
 
 
 dirname = os.path.abspath(os.path.dirname(__file__))
+
+deps = Path('requirements.txt').read_text().splitlines()
+
+path_to_change = os.path.join(dirname, "src")
+
+os.chdir(path_to_change)
+
+dirname = path_to_change
+
 
 with codecs.open(
     os.path.join(dirname, "undetected_chromedriver", "__init__.py"),
@@ -19,34 +28,15 @@ with codecs.open(
 
 description = (
     "Selenium.webdriver.Chrome replacement with compatiblity for Brave, and other Chromium based browsers.",
-    "Not triggered by CloudFlare/Imperva/hCaptcha and such.",
-    "NOTE: results may vary due to many factors. No guarantees are given, except for ongoing efforts in understanding detection algorithms.",
 )
 
 setup(
     name="undetected-chromedriver",
     version=version,
-    packages=["src"],
-    install_requires=[
-        "selenium>=4.9.0",
-        "requests",
-        "websockets",
-    ],
-    package_data={"undetected_chromedriver": [os.path.join("example", "example.py")]},
-    url="https://github.com/ultrafunkamsterdam/undetected-chromedriver",
+    install_requires=deps,
+    py_modules=['utils'],
+    url="https://github.com/sellamanic/FlareSolverr",
     license="GPL-3.0",
-    author="UltrafunkAmsterdam",
-    author_email="info@blackhat-security.nl",
+    author="xkjjdsasuw",
     description=description,
-    long_description=open(os.path.join(dirname, "README.md"), encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
 )
