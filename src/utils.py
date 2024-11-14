7df80935d973d2f4c0a0cdd120a48bff8124b732
sellamanic <108454368+sellamanic@users.noreply.github.com>
2024-11-14 13:08:56 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -143,7 +143,7 @@ def get_webdriver(proxy: dict = None, display=False, chrome=None) -> WebDriver:
     # https://github.com/FlareSolverr/FlareSolverr/issues/782
     # https://github.com/microsoft/vscode/issues/127800#issuecomment-873342069
     # https://peter.sh/experiments/chromium-command-line-switches/#use-gl
-    options.add_argument('--use-gl=swiftshader')
+    # options.add_argument('--use-gl=swiftshader')
 
     language = os.environ.get('LANG', None)
     if language is not None:
